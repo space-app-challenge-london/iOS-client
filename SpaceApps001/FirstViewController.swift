aba36069249346f2c3b4a708c8317cd780404ac0
Brett <bratt.neumayer@gmail.com>
2017-04-29 14:55:31 +0100

    Initial Commit

---
+++ SpaceApps001/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SpaceApps001
+//
+//  Created by Bratt Neumayer on 4/29/17.
+//  Copyright © 2017 Bratt Neumayer. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
